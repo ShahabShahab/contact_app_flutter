fec1c10c9c8804b04b3dccb895b531d1f493ff66
Shahab Golpaiegan <shahabgolpaiegan@gmail.com>
2021-12-11 14:05:19 +0330

    init db is almost done

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Directory appDocumentDir =
+      await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDocumentDir.path);
   runApp(const MyApp());
 }
 
